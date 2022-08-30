a9fd4894d024df1b0c6bade5dbd2e79847dd9456
Rosihan Ardiansyah <rosihan17@gmail.com>
2022-08-30 21:04:25 +0800

    install cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express =  require ("express");
+const cors = require ("cors");
 const route = require("./routes/apiRoutes");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(route);
 
 app.listen(5000, ()=>console.log('server running....'));
 
 
-module.exports = app;  
+module.exports = app;   
